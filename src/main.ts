5562aeb6be4eb89ebebd90f1a1f945caad9dccf6
KarineValerioLima <156118702+KarineValerioLima@users.noreply.github.com>
2024-01-30 17:35:52 -0300

    Revisão da aula

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import {db, firestore} from '../banco_de_dados/firebase';
+
+const app = express();
+
+
+
+app.listen(300,function (){
+    console.log("serviço rodando na porta http://locelhost:3000");
+});
